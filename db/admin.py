328d940cdbdcdffa21ba9a8e1f5ac1f051965840
joelarkman <joelarkman@gmail.com>
2021-03-24 17:09:50 +0000

    Added models to admin

--- db/admin.py
+++ db/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+
+class BaseModelAdmin(admin.ModelAdmin):
+    readonly_fields = (
+        'date_created',
+        'date_modified'
+    )
+
+
+app = apps.get_app_config('db')
+for model_name, model in app.models.items():
+    admin.site.register(model, BaseModelAdmin)
