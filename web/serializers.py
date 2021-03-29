63aee2fc4ee882c6fce75ee92df3208f50677c92
joelarkman <joelarkman@gmail.com>
2021-03-29 11:54:05 +0100

    Implemented serverside datatables approach to sample search page

--- web/serializers.py
+++ web/serializers.py
@@ -5,9 +5,22 @@ from db.models import Sample, Samplesheet, Run
 
 class SampleListSerializer(serializers.ModelSerializer):
     samplesheets = serializers.SerializerMethodField()
+    panel = serializers.SerializerMethodField()
+    completed_at = serializers.SerializerMethodField()
+    qc_status = serializers.SerializerMethodField()
 
     def get_samplesheets(self, sample):
         return sample.samplesheets.last().run.worksheet
+
+    def get_panel(self, sample):
+        return sample.samplesheets.last().run.pipeline_version.pipeline.name
+
+    def get_completed_at(self, sample):
+        date = sample.samplesheets.last().run.completed_at
+        return date.strftime("%Y-%m-%d")
+
+    def get_qc_status(self, sample):
+        return sample.samplesheets.last().run.get_qc_status_display()
 
     class Meta:
         model = Sample
