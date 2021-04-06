9425797d32c55c94971283670d09560c2b7eb167
Theo Cole <theo.cole@nhs.net>
2021-04-06 12:54:08 +0100

    fix dict typo

--- VariantViewer/settings.py
+++ VariantViewer/settings.py
@@ -101,8 +101,8 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
-    
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    {
+	'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
