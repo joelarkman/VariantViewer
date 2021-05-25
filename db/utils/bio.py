2477bbfe70ab316ef9e672be11840d7676b08f0d
Theo Cole <theo.j.cole@gmail.com>
2021-05-25 16:04:32 +0100

    fix missing comma

--- db/utils/bio.py
+++ db/utils/bio.py
@@ -139,7 +139,7 @@ class VariantManager:
                     "HGVSc",
                     "HGVSp",
                     "Consequence",
-                    "IMPACT"
+                    "IMPACT",
                     "CANONICAL",
                 ],
                 dtypes={
