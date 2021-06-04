b0cc6fd1af633878ffa452d705a596f0bd2ad1e1
joelarkman <joelarkman@gmail.com>
2021-06-04 17:50:16 +0100

    Implemented draft user profile functionality

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
 from accounts.models import User
+from db.models import Section
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -46,3 +47,32 @@ class CustomUserCreationForm(UserCreationForm):
             self.add_error(
                 'email', 'A user with that email address already exists.')
         return cleaned_data
+
+
+class UpdateProfileForm(CustomUserCreationForm):
+    """Form allowing editing of certain user fields - not username.
+    Since this inherits from a UserCreationForm (grandparent) must not provide
+    password fields either which are included by default.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(UpdateProfileForm, self).__init__(*args, **kwargs)
+        del self.fields['password1']
+        del self.fields['password2']
+
+    default_section = forms.ModelChoiceField(
+        queryset=Section.objects.all(),
+        widget=forms.Select(
+            attrs={
+                'class': 'ui dropdown'
+            }
+        )
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name',
+            'last_name',
+            'default_section'
+        )
