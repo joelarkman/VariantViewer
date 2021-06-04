a6a1f108e5a363f44f9ba2cee0225e2d1e944e0a
joelarkman <joelarkman@gmail.com>
2021-06-04 17:52:26 +0100

    Added lastupdated utility function

--- web/utils/model_utils.py
+++ web/utils/model_utils.py
@@ -1,4 +1,6 @@
 from django.db import models
+from easyaudit.models import CRUDEvent
+import json
 
 
 class BaseModel(models.Model):
@@ -10,3 +12,33 @@ class BaseModel(models.Model):
     class Meta:
         abstract = True
         app_label = 'web'
+
+
+def LastUpdated(object, fields=None):
+
+    events = {}
+    cruds = CRUDEvent.objects.filter(
+        object_repr=object, event_type=CRUDEvent.UPDATE)
+
+    latest = cruds.first()
+
+    events['latest'] = {'type': 'update',
+                        'user': latest.user, 'datetime': latest.datetime}
+
+    if fields:
+        fields = fields if isinstance(fields, list) else [fields]
+        field_latest = {}
+        for field in fields:
+            temp = []
+            for crud in cruds:
+                data = json.loads(crud.changed_fields)
+                if data and field in data:
+                    temp.append({'type': 'update', 'user': crud.user, 'datetime': crud.datetime,
+                                 'value_previous': data[field][0], 'value': data[field][1]})
+
+            temp = sorted(temp, key=lambda k: k['datetime'], reverse=True)[0]
+            field_latest[field] = temp
+
+        events['fields'] = field_latest
+
+    return events
