2f80ec2a7bf003d549f0acbb085d332612acb3f9
joelarkman <joelarkman@gmail.com>
2021-07-29 16:58:49 +0100

    Updated celery.py to correct name

--- VariantViewer/celery.py
+++ VariantViewer/celery.py
@@ -5,6 +5,6 @@ from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VariantViewer.settings.local')
 
 app = Celery(
-    'change', broker=settings.CELERY_BROKER_URL)
+    'VariantViewer', broker=settings.CELERY_BROKER_URL)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
