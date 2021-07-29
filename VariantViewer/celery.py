30271baf347e007facddd34b5915c6da37f8bf6e
joelarkman <joelarkman@gmail.com>
2021-07-29 17:09:22 +0100

    Formatting changes

--- VariantViewer/celery.py
+++ VariantViewer/celery.py
@@ -1,10 +1,11 @@
 import os
+
 from celery import Celery
 from django.conf import settings
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VariantViewer.settings.local')
 
-app = Celery(
-    'VariantViewer', broker=settings.CELERY_BROKER_URL)
+app = Celery('VariantViewer', broker=settings.CELERY_BROKER_URL)
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
